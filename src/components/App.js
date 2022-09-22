a2ab887544e19abedfc742aa58464aca6ff449ec
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-09-23 00:19:42 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header";
+import About from "./About";
+import ArticleList from "./ArticleList";
+import Article from "./Article";
 
 console.log(blogData);
 
@@ -8,8 +12,13 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header />
+      <About />
+      <ArticleList />
+      <Article />
     </div>
   );
 }
 
 export default App;
+
